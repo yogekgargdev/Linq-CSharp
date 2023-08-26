59bb2ba5e9912205b1422a634795179e617a77e2
Yogek.Garg <Yogek.Garg@DESKTOP-56R8DEK>
2023-08-26 23:31:25 +0530

    foreach is applicable on the type IEnumerable

---
+++ IEnumerableExample/IEnumerableExample/Program.cs
@@ -0,0 +1,29 @@
+﻿// Example for IEnumerable
+
+Person p = new Person("Yogek", "Garg", "Punjab");
+// foreach is applicable on the type "IEnumerable"
+foreach (string z in p.NameIEnumerable())
+{
+    Console.WriteLine(z);
+}
+
+Console.ReadLine();
+public class Person
+{
+    public string FirstName, MiddleName, LastName;
+
+    public Person(string firstName, string middleName, string lastName)
+    {
+        FirstName = firstName;
+        MiddleName = middleName;
+        LastName = lastName;
+    }
+    public IEnumerable<string> NameIEnumerable()
+    {
+        yield return FirstName;
+        yield return MiddleName;
+        yield return LastName;
+    }
+}
+
+// Example for IEnumerable
