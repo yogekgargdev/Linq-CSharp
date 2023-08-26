461c819f9104e9e090a7958f803338cbaa5758b3
Yogek.Garg <Yogek.Garg@DESKTOP-56R8DEK>
2023-08-26 23:39:29 +0530

    IEnumerable inherit into a class

--- IEnumerableExample/IEnumerableExample/Program.cs
+++ IEnumerableExample/IEnumerableExample/Program.cs
@@ -1,11 +1,23 @@
 ﻿// Example for IEnumerable
 
+using System.Collections;
+
 Person p = new Person("Yogek", "Garg", "Punjab");
 // foreach is applicable on the type "IEnumerable"
 foreach (string z in p.NameIEnumerable())
 {
     Console.WriteLine(z);
 }
+
+
+////////////////////////////////////////////////
+
+Car CarObj = new Car(1, 2, 3);
+foreach(int i in CarObj)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("FirstOrDefault: " + CarObj.FirstOrDefault());
 
 Console.ReadLine();
 public class Person
@@ -26,4 +38,28 @@ public class Person
     }
 }
 
-// Example for IEnumerable
+
+// Example for IEnumerable when a class inherits it
+public class Car : IEnumerable<int>
+{
+    public int Id, Id1, Id2;
+    public Car(int id, int id1, int id2)
+    {
+        Id = id;
+        Id1 = id1;
+        Id2 = id2;
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        yield return Id;
+        yield return Id1;
+        yield return Id2;
+
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
